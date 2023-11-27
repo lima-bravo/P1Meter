b60b90b7bb1146c83708ab09ae7aefe5e94d7af7
Lodewijk Bonebakker <lodewijk@bonebakker.org>
2023-11-27 08:38:19 +0100

    updated syncP1.sh script to match current filesystem layout

--- syncP1.sh
+++ syncP1.sh
@@ -3,11 +3,11 @@
 # Synchronize the SmartMeter directory on p1pi to iMac
 #
 #
-COMMAND=/usr/bin/rsync
+COMMAND=/opt/local/bin/rsync
 SWITCHES="-avuz"
 FLAGS="--progress --stats"
 SOURCE="pi@p1pi:/home/pi/Programming/SmartMeter/"
-DEST="/zfs/lodewijk/Programming/SmartMeter/"
+DEST="/zfs/lodewijk/Programming/HomeAutomation/SmartMeter/"
 #
 # execute command
 ${COMMAND} ${SWITCHES} ${SOURCE} ${DEST} ${FLAGS}
